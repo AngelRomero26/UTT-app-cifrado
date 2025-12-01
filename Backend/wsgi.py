d671e68fe180b3a1ee46411e580cdad0f8ab0171
AngelRomero26 <a3523110195@uttehuacan.edu.mx>
2025-11-30 23:25:43 -0600

    tercer commit

--- Backend/wsgi.py
+++ Backend/wsgi.py
@@ -1,10 +1,13 @@
 import sys
 import os
 
-# Ruta del proyecto
-project_path = os.path.dirname(os.path.abspath(__file__))
-if project_path not in sys.path:
-    sys.path.append(project_path)
+# → Ruta absoluta al directorio de tu proyecto
+project_home = '/home/AngelRomero26/UTT-app-cifrado'
 
-# Importar Flask app
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+os.chdir(project_home)
+
+# Importa la app Flask como application (requisito de WSGI)
 from app import app as application
